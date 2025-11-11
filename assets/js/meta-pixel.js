8836bca46fe9873ee136933b9169dfd29b99444a
DestinyRL <146444654+DestinyRL@users.noreply.github.com>
2025-11-11 22:38:22 +0100

    Add files via upload

---
+++ assets/js/meta-pixel.js
@@ -0,0 +1,12 @@
+/* Meta / Facebook Pixel - centralized file
+   Pixel ID: 1560156251820544
+   This file can be referenced from all HTML pages with:
+   <script src="assets/js/meta-pixel.js"></script>
+*/
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1560156251820544');
+fbq('track', 'PageView');
